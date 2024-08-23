64adf06fcca47082578ed2c643ac4e7c66acda30
Thomas Platts <67462990+TPlatts04@users.noreply.github.com>
2024-08-23 22:49:55 +0100

    Create html files and routes

--- app.py
+++ app.py
@@ -4,5 +4,17 @@ import os
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
 
+@app.route("/")
+def homepage():
+    return render_template("home.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
