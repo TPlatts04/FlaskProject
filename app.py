de5a0ff0821c4ff9b0eebf753df7ed2eecf7b9b9
Thomas Platts <67462990+TPlatts04@users.noreply.github.com>
2024-08-26 12:50:37 +0100

    Saved data to database

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask, render_template, redirect, url_for, session, request, flash
+from flask_sqlalchemy import SQLAlchemy
 from storeUser import *
 import os
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
 
 @app.route("/")
 def homepage():
@@ -13,10 +27,15 @@ def homepage():
 def login():
     if request.method == "POST":
         username, password = request.form["username"], request.form["password"]
-        try:
-            userLogin(username, password)
-            session["profileUsername"] = username
-        except ValueError:
+        session["username"], session["password"] = username, password
+        found_user = users.query.filter_by(name=username).first()
+        if found_user:
+            if password == found_user.password:
+                session["profileUsername"] = found_user.name
+            else:
+                flash("Incorrect Password. Please try again.")
+                return render_template("login.html")
+        else:
             flash("There is no account under this name. Please create one.")
             return render_template("login.html")
         return render_template("home.html")
@@ -27,11 +46,20 @@ def login():
 def createAccount():
     if request.method == "POST":
         username, password = request.form["create-username"], request.form["create-password"]
-        try:
-            userCreate(username, password) 
-        except ValueError:
-            flash("Password is Ineligible. Please try a new one.", "info")
-            return render_template("createAccount.html")
+        session["username"], session["password"] = username, password
+        found_user = users.query.filter_by(name=username).first()
+        if found_user:
+            flash("A user with this username already exists.", "info")
+            return redirect(url_for("createAccount"))
+        else:
+            try:
+                usr = users(username, password)
+                userCreate(usr)
+                db.session.add(usr)
+                db.session.commit()
+            except ValueError:
+                flash("Password is Ineligible. Please try a new one.", "info")
+                return render_template("createAccount.html")
         flash("Account created!")
         return render_template("createAccount.html")
     return render_template("createAccount.html")
@@ -41,5 +69,6 @@ def about():
     return render_template("about.html")
 
 if __name__ == "__main__":
-    main()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
--- storeUser.py
+++ storeUser.py
@@ -1,45 +1,11 @@
-import csv
-import os
 import re
+from app import db, session
 
-FILENAME = "csvDB.csv"
-FIELDNAMES = ['username', 'password']
-
-data = [{
-    "username": "",
-    "password": ""
-}]
-
-def main():
-    if os.path.isfile(FILENAME):
-        pass
-    else:
-        with open(FILENAME, "a", newline="") as file:
-            writer = csv.DictWriter(file, fieldnames=FIELDNAMES)
-            writer.writeheader()
-            pass
-
-def userLogin(username, password):
-    with open(FILENAME, "r", newline="") as file:
-        lines = file.readlines()
-        lineChecker = 0
-        for line in lines:
-            if username in line and password in line:
-                pass
-            else:
-                lineChecker += 1
-                if lineChecker == len(lines):
-                    raise ValueError
-
-def userCreate(username, password):
+def userCreate(usrClass):
     # Check if the password matches the requirements requested on CreateAccount page
-    match = re.match('(?=.*[A-Z]){1,}.{4,}[\d]{3,}', password)
+    match = re.match('(?=.*[A-Z]){1,}.{4,}[\d]{3,}', usrClass.password)
     if match:
-        data[0]["username"], data[0]["password"] = username, password
-        with open(FILENAME, "a", newline="") as file:
-            writer = csv.DictWriter(file, fieldnames=FIELDNAMES)
-            for row in data:
-                writer.writerow(row)
+        pass
     else:
         raise ValueError
         
